9d424d6f7dc8436e9c3c5a81fe965e6a9d2bc3e3
HadeSatanist <ap66484@gmail.com>
2022-09-19 22:05:20 -0300

    menu hamburguer

--- SIG2/src/main/resources/static/js/menu.js
+++ SIG2/src/main/resources/static/js/menu.js
@@ -1,10 +1,8 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
 }
+
+btnMobile.addEventListener('click', toggleMenu);
