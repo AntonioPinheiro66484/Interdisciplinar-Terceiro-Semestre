9863ad658513736df6505b24ad287982a616eaeb
HadeSatanist <ap66484@gmail.com>
2022-10-17 16:18:47 -0300

    aa

--- SIG2/src/main/resources/static/js/Darkmode.js
+++ SIG2/src/main/resources/static/js/Darkmode.js
@@ -1,4 +1,4 @@
-a//We're going to use "check" to get the ckeckbox element
+//We're going to use "check" to get the ckeckbox element
 const check =document.getElementById("check")
 
 //If darkMode doesn’t exist in the LocalStorage, create it. False by default
@@ -14,15 +14,18 @@ function checkStatus(){
     if (localStorage.getItem('darkMode')==="true"){
         check.checked = true;                                       //the checkbox is checked (if you load the page by default it isn’t)
         document.getElementsByTagName("body")[0].style.color="#FFF";
-
-        document.getElementsByTagName("p")[0].style.color="#FFF";   //the thext in the p is set to white
+        document.getElementsByTagName("p")[0].style.color="#FFF";   
+        document.getElementsByTagName("h3")[0].style.color="black";  //the thext in the p is set to white
         document.body.style.backgroundColor = "#181A1B";            //the backgornd is set to a dark grey
+
+
     }else{
         check.checked = false;
         document.getElementsByTagName("body")[0].style.color="black";
-
         document.getElementsByTagName("p")[0].style.color="black";  //same CSS changes but for light mode
         document.body.style.backgroundColor = "#FFF";
+        document.getElementById("product-title").style.color="black";
+        document.getElementById("product-price").style.color="black";
     }
 }
 
@@ -30,14 +33,56 @@ function changeStatus(){                                            //This funct
     if (localStorage.getItem('darkMode')==="true"){                 //if darkMode was active and this function is called it means the user now wants light
         localStorage.setItem('darkMode', "false");                  //so we set it to false, to indicate we are in light mode
         document.getElementsByTagName("body")[0].style.color="black"; 
-
-        document.getElementsByTagName("p")[0].style.color="black"; //same CSS changes as in checkStatus
+        document.getElementsByTagName("p")[0].style.color="black";  //same CSS changes as in checkStatus
         document.body.style.backgroundColor = "#FFF";
+        document.getElementById("product-title").style.color="black";
+        document.getElementById("product-price").style.color="black";
+        
+        //if ( document.getElementsByName("produto-titulo").classList.contains('product-title')) {
+        //document.getElementsByName('produto-titulo').classList.remove("product-title");
+		//document.getElementsByName('produto-titulo').classList.add("product-title-escuro");
+		//}
+
+
+		const titulos = document.getElementsByName('produto-titulo')
+		
+		for(let i = 0; i < titulos.length; i++){
+			if(titulos[i].classList.contains('product-title-escuro')){
+				titulos[i].classList.remove("product-title-escuro");
+				titulos[i].classList.add("product-title");
+			}
+		}
+		console.log(titulos[0].classList);
+
+        //console.log(document.getElementById("product-price").style)
+
     } else{
         localStorage.setItem('darkMode', "true");                   //same code but adapted for dark theme
         document.getElementsByTagName("body")[0].style.color="#FFF";
-
-        document.getElementsByTagName("p")[0].style.color="#FFF";
+        document.getElementsByTagName("p")[0].style.color="#FFF"; 
         document.body.style.backgroundColor = "#181A1B";
+        document.getElementById("product-title").style.color="#FFF";
+        document.getElementById("product-price").style.color="#FFF";
+		
+		//if ( document.getElementsByName("produto-titulo").classList.contains('product-title-escuro')) {
+        //document.getElementsByName('produto-titulo').classList.remove("product-title-escuro");
+		//document.getElementsByName('produto-titulo').classList.add("product-title");
+		//}
+
+        console.log(precos[0].classList); 
+
+		const titulos = document.getElementsByName('produto-titulo')
+		for(let i = 0; i < titulos.length; i++){
+			if(titulos[i].classList.contains('product-title')){
+				titulos[i].classList.remove("product-title");
+				titulos[i].classList.add("product-title-escuro");
+			}
+		}
+		console.log(titulos[0].classList);
+
+        
+		//console.log(document.getElementById("product-price").style)
+
+
     }
 }
