fada89ec60a63116bf636687abe5c369812d826f
HadeSatanist <ap66484@gmail.com>
2022-10-19 17:01:03 -0300

    atualização de nome de darkmode

--- SIG2/src/main/resources/static/js/Darkmode.js
+++ SIG2/src/main/resources/static/js/Darkmode.js
@@ -14,6 +14,7 @@ function checkStatus(){
     if (localStorage.getItem('darkMode')==="true"){
         check.checked = true;                                       //the checkbox is checked (if you load the page by default it isn’t)
         document.getElementsByTagName("body")[0].style.color="#FFF";
+
         document.getElementsByTagName("p")[0].style.color="#FFF";   //the thext in the p is set to white
         document.body.style.backgroundColor = "#181A1B";            //the backgornd is set to a dark grey
 
@@ -21,6 +22,7 @@ function checkStatus(){
     }else{
         check.checked = false;
         document.getElementsByTagName("body")[0].style.color="black";
+
         document.getElementsByTagName("p")[0].style.color="black";  //same CSS changes but for light mode
         document.body.style.backgroundColor = "#FFF";
         document.getElementById("product-title").style.color="black";
@@ -32,6 +34,7 @@ function changeStatus(){                                            //This funct
     if (localStorage.getItem('darkMode')==="true"){                 //if darkMode was active and this function is called it means the user now wants light
         localStorage.setItem('darkMode', "false");                  //so we set it to false, to indicate we are in light mode
         document.getElementsByTagName("body")[0].style.color="black"; 
+
         document.getElementsByTagName("p")[0].style.color="black"; //same CSS changes as in checkStatus
         document.body.style.backgroundColor = "#FFF";
         document.getElementById("product-title").style.color="black";
@@ -41,24 +44,15 @@ function changeStatus(){                                            //This funct
         //document.getElementsByName('produto-titulo').classList.remove("product-title");
 		//document.getElementsByName('produto-titulo').classList.add("product-title-escuro");
 		//}
-        const precos = document.getElementById('preco-produto')
-        for(let i = 0; i<precos.length;i++){
-            if(precos[i].classList.contains('product-price-escuro')){
-				precos[i].classList.remove('product-price-escuro');
-				precos[i].classList.add('preco-produto');
-			}
-        }
-        console.log(precos[0].classList);
 
-		const titulos = document.getElementsByName('produto-titulo')
+		const titulos = document.getElementsByName("produto-titulo")
 		
 		for(let i = 0; i < titulos.length; i++){
-			if(titulos[i].classList.contains('product-title-escuro')){
+			if(titulos[i].classList.contains("product-title-escuro")){
 				titulos[i].classList.remove("product-title-escuro");
 				titulos[i].classList.add("product-title");
 			}
 		}
-        
 		console.log(titulos[0].classList);
         //console.log(document.getElementById("product-price").style)
     } else{
@@ -83,17 +77,26 @@ function changeStatus(){                                            //This funct
 			}
 		}
 		console.log(titulos[0].classList);
-
-        const precos = document.getElementsByName('produto-preco')
-		for(let i = 0; i < precos.length; i++){
-			if(precos[i].classList.contains('product-price')){
-				precos[i].classList.remove('product-price');
-				precos[i].classList.add('product-price-escuro');
-			}
-		}
-		console.log(precos[0].classList);
-		//console.log(document.getElementById("product-price").style)
+	//console.log(document.getElementById("product-price").style)
 
 
+    
+    const precos=document.getElementsByName("produto-preco")
+    for(let i = 0; i < precos.length; i++){
+        if(precos[i].classList.contains("product-price")){
+            precos[i].classList.remove("product-price");
+            precos[i].classList.add("product-price-escuro");
+        }
     }
+
+    for(let i = 0; i < precos.length; i++){
+        if(precos[i].classList.contains("product-price")){
+            precos[i].classList.remove("product-price");
+            precos[i].classList.add("product-price-escuro");
+        }
+    }
+
+    console.log(precos[0].classList);
+    }
+
 }
