387e433fe00d3d869db503561f40b0a9439d056f
HadeSatanist <ap66484@gmail.com>
2022-11-03 15:58:26 -0300

    make it better

--- SIG2/src/main/java/com/fatec/SIG2/security/ConfiguracaoDeSeguranca.java
+++ SIG2/src/main/java/com/fatec/SIG2/security/ConfiguracaoDeSeguranca.java
@@ -1,5 +1,4 @@
 package com.fatec.SIG2.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
