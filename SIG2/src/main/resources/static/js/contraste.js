d1f9e092bd2a7363081db5d66018019f980cf11e
HadeSatanist <ap66484@gmail.com>
2022-11-03 16:17:09 -0300

    Implementação do modo contraste

--- SIG2/src/main/resources/static/js/contraste.js
+++ SIG2/src/main/resources/static/js/contraste.js
@@ -1,71 +1,100 @@
-var link_css = "/css/estiloLoja.css";
+//We're going to use "check" to get the ckeckbox element
+const check = document.getElementById("check")
 
-				
-if(getCookie2() == "contraste"){
-    link_css = "/css/contraste.css";
-}
-else if(getCookie() == "default"){
-    link_css = "/css/estiloLoja.css";
+//If darkMode doesn’t exist in the LocalStorage, create it. False by default
+if (localStorage.getItem('darkMode') === null) {
+    localStorage.setItem('darkMode', "false");
 }
 
-$(document).ready(function(){
-    
-    $("#modoEscuro").click(function(){
-        setCookie("contraste");
-        location.reload();				
-    });
-    
-    $("#modoClaro").click(function(){
-        setCookie("default")
-        location.reload();									
-    });
-    
-});
+//checkStatus is only called one time in the program, when you reload the page
+//It gives the page it's default look, depening on waht darkMode is set to
+checkStatus()
 
-$("head").append("<link rel=stylesheet href="+ " "+ link_css +" " +"type=text/css>");
+function checkStatus() {
+    if (localStorage.getItem('darkMode') === "true") {
+        check.checked = true;                                       //the checkbox is checked (if you load the page by default it isn’t)
+        document.getElementsByTagName("body")[0].style.color = "#FFF";
 
-function setCookie(value){
-    var data = new Date();
-    data.setTime(data.getTime() + 600000);
-    
-    //cookies structure
-    document.cookie = "contraste="+value+"; expires="+data.toUTCString()+
-        "; path=/";
-    
-}
+        document.getElementsByTagName("p")[0].style.color = "#FFF";   //the thext in the p is set to white
+        document.body.style.backgroundColor = "#181A1B";            //the backgornd is set to a dark grey
 
-function getCookie(){
-    var cookie = document.cookie.split("=");
-    
-    return cookie[1];
-}
 
-function getCookie2(){
-    var nameEQ = "contraste=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    } else {
+        check.checked = false;
+        document.getElementsByTagName("body")[0].style.color = "black";
+
+        document.getElementsByTagName("p")[0].style.color = "black";  //same CSS changes but for light mode
+        document.body.style.backgroundColor = "#FFF";
+        document.getElementById("product-title").style.color = "black";
+        document.getElementById("product-price").style.color = "black";
     }
-    return undefined;
-
 }
 
-//BOTOES ROLAGEM CARROSSEL
-const groupCards = [...document.querySelectorAll('.conjuntoCard')];
-const btnAft = [...document.querySelectorAll('.btnAft')];
-const btnBef = [...document.querySelectorAll('.btnBef')]
+function changeStatus() {                                            //This function gets called every time the checkbox is clicked
+    if (localStorage.getItem('darkMode') === "true") {                 //if darkMode was active and this function is called it means the user now wants light
+        localStorage.setItem('darkMode', "false");                  //so we set it to false, to indicate we are in light mode
+        document.getElementsByTagName("body")[0].style.color = "black";
 
-groupCards.forEach((item, i ) =>{
-    let groupCardsDimen = item.getBoundingClientRect();
-    let groupCardsLength = groupCardsDimen.width;
+        document.getElementsByTagName("p")[0].style.color = "black"; //same CSS changes as in checkStatus
+        document.body.style.backgroundColor = "#FFF";
+        document.getElementById("product-title").style.color = "black";
+        document.getElementById("product-price").style.color = "black";
 
-    btnAft[i].addEventListener('click', () =>{
-        item.scrollLeft += groupCardsLength;
-    })
+        //if ( document.getElementsByName("produto-titulo").classList.contains('product-title')) {
+        //document.getElementsByName('produto-titulo').classList.remove("product-title");
+        //document.getElementsByName('produto-titulo').classList.add("product-title-escuro");
+        //}
 
-    btnBef[i].addEventListener('click', () => {
-        item.scrollLeft -= groupCardsLength;
-    })
-})
+        const titulos = document.getElementsByName("produto-titulo")
+        for (let i = 0; i < titulos.length; i++) {
+            if (titulos[i].classList.contains("product-title-escuro")) {
+                titulos[i].classList.remove("product-title-escuro");
+                titulos[i].classList.add("product-title");
+            }
+        }
+        console.log(titulos[0].classList);
+
+        const precos = document.getElementsByName("produto-preco")
+        for (let i = 0; i < precos.length; i++) {
+            if (precos[i].classList.contains("product-price-escuro")) {
+                precos[i].classList.remove("product-price-escuro");
+                precos[i].classList.add("product-price");
+            }
+        }
+        console.log(precos[0].classList);
+    } else {
+        localStorage.setItem('darkMode', "true");                   //same code but adapted for dark theme
+        document.getElementsByTagName("body")[0].style.color = "#FFF";
+
+        document.getElementsByTagName("p")[0].style.color = "#FFF";
+        document.body.style.backgroundColor = "#181A1B";
+        document.getElementById("product-title").style.color = "#FFF";
+        document.getElementById("product-price").style.color = "#FFF";
+
+        //if ( document.getElementsByName("produto-titulo").classList.contains('product-title-escuro')) {
+        //document.getElementsByName('produto-titulo').classList.remove("product-title-escuro");
+        //document.getElementsByName('produto-titulo').classList.add("product-title");
+        //}
+
+        const titulos = document.getElementsByName('produto-titulo')
+        for (let i = 0; i < titulos.length; i++) {
+            if (titulos[i].classList.contains('product-title')) {
+                titulos[i].classList.remove("product-title");
+                titulos[i].classList.add("product-title-escuro");
+            }
+        }
+        console.log(titulos[0].classList);
+        //console.log(document.getElementById("product-price").style)
+
+        const precos = document.getElementsByName("produto-preco")
+        for (let i = 0; i < precos.length; i++) {
+            if (precos[i].classList.contains("product-price")) {
+                precos[i].classList.remove("product-price");
+                precos[i].classList.add("product-price-escuro");
+            }
+        }
+
+        console.log(precos[0].classList);
+    }
+
+}
