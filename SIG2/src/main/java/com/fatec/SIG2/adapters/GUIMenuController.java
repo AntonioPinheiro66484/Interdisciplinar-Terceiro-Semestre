13dcd855887307c23e6acdd5f885d8226872072a
HadeSatanist <ap66484@gmail.com>
2022-11-16 16:58:26 -0300

    Pagina de adm e cliente adicionadas na guia controller

--- SIG2/src/main/java/com/fatec/SIG2/adapters/GUIMenuController.java
+++ SIG2/src/main/java/com/fatec/SIG2/adapters/GUIMenuController.java
@@ -5,6 +5,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GUIMenuController {
+	@GetMapping("/paginaCliente")
+	public ModelAndView paginacliente() {
+		return new ModelAndView("paginaCliente");
+	}
 	@GetMapping("/login")
 	public ModelAndView autenticacao() {
 		return new ModelAndView("paginaLogin");
@@ -12,41 +16,49 @@ public class GUIMenuController {
 
 	@GetMapping("/")
 	public ModelAndView home() {
-		return new ModelAndView("paginaMenu");
+		return new ModelAndView("paginaMenuAdm");
 	}
 	
 	@GetMapping("/acessibilidade")
 	public ModelAndView acess() {
 		return new ModelAndView("acessibilidade");
 	}
+
 	@GetMapping("/paginaProduto")
 	public ModelAndView produto() {
 		return new ModelAndView("paginaProduto");
 	}
+
 	@GetMapping("/paginaProduto2")
 	public ModelAndView produto2() {
 		return new ModelAndView("paginaProduto2");
 	}	
+	
 	@GetMapping("/paginaProduto1")
 	public ModelAndView produto1() {
 		return new ModelAndView("paginaProduto1");
 	}	
+
 	@GetMapping("/paginaProduto3")
 	public ModelAndView produto3() {
 		return new ModelAndView("paginaProduto3");
 	}	
+
 	@GetMapping("/paginaProduto4")
 	public ModelAndView produto4() {
 		return new ModelAndView("paginaProduto4");
 	}	
+
 	@GetMapping("/paginaProduto5")
 	public ModelAndView produto5() {
 		return new ModelAndView("paginaProduto5");
 	}	
+
 	@GetMapping("/paginaProduto6")
 	public ModelAndView produto6() {
 		return new ModelAndView("paginaProduto6");
 	}	
+	
 	@GetMapping("/paginaProduto7")
 	public ModelAndView produto7() {
 		return new ModelAndView("paginaProduto7");
